ed070c1bddc94d59b14340b65ea7f0cbe500ef4e
tempura327 <cks6658324@yahoo.com.tw>
2021-11-15 22:03:55 +0800

    [page] added News and NewsList.

---
+++ src/views/News.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col>
+        
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+
+  export default {
+    name: 'Home',
+    components: {
+    
+    },
+    data(){
+      return{
+      
+      }
+    },
+    methods:{
+      
+    }
+  }
+</script>

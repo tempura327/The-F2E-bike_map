6d9e1f3157e6050e06500b6fd772e4d0f319ab9b
tempura327 <cks6658324@yahoo.com.tw>
2021-11-15 22:00:39 +0800

    set router and store

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,38 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+const Home = () => import('../views/Home.vue');
+const News = () => import('../views/News.vue');
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'Home',
-    component: Home
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path:'/news',
+    name:'News',
+    component:News
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+  routes,
+  props:{
+    default: true,
+  },
+  scrollBehavior(){
+    return {
+      x:0,
+      y:0,
+      behavior: 'smooth'
+    }
+
+  }
 })
 
 export default router

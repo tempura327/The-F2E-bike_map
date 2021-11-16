844331c649de3fcd71d1dd9c561f2bd7913f02c8
tempura327 <cks6658324@yahoo.com.tw>
2021-11-16 21:54:13 +0800

    [page] modified Home.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@
     </div>
 
     <b-container>
-      <b-row>
+      <b-row class="mb-28">
         <b-col class="d-flex justify-content-center">
           <b-button class="btn-big bg-primary">自行車路線</b-button>
         </b-col>
@@ -22,12 +22,34 @@
           <b-button class="btn-big bg-secondary">單車租借</b-button>
         </b-col>
       </b-row>
+
+      <b-row class=" mb-15">
+        <b-col>
+          <div class="middle-title text-center">最新公告</div>
+        </b-col>
+      </b-row>
+
+      <b-row class="mb-30">
+        <b-col>
+          <ul class="mb-12">
+            <li v-for="(item, index) in news" :key="index" class="news-item">
+              <h6 class="h6 text-light">{{item.date}}</h6>
+              <router-link to="/" class="a">{{item.title}}</router-link>
+              <h6 class="h6 text-light"><font-awesome-icon icon="angle-right"></font-awesome-icon></h6>
+            </li>
+          </ul>
+          <div class="text-center">
+            <button class="btn-small bg-primary">更多消息</button>
+          </div>
+        </b-col>
+      </b-row>
     </b-container>
 
   </div>
 </template>
 
 <script>
+  import News from '../config/News.json';
 
   export default {
     name: 'Home',
@@ -36,7 +58,7 @@
     },
     data(){
       return{
-      
+        news:News,
       }
     },
     methods:{
@@ -71,5 +93,12 @@
     margin: 0 auto;
     width: 25%;
       
+  }
+
+  .news-item{
+    display: flex;
+    justify-content: space-between;
+    padding: 24px 40px;
+    border-bottom: 1px solid #C8C8C8;
   }
 </style>
